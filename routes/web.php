6d31c2440719de7a50f8cc8c3f21e4d09a8607f9
victorsteven <chikodi543@gmail.com>
2018-10-07 22:20:07 +0100

    queue worker added

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Mail;
+use App\Mail\TestMail;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('check-queue', function () {
+    Mail::to('chikodi543@gmail.com')->send(new TestMail());
+
+    return 'working';
 });
